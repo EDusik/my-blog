6889ee116b0077fd77113d677f45ea43f00efc84
EDusik <eduardodusik@gmail.com>
2021-02-02 18:31:41 -0300

    chore: adding styled-components lib and some changes in Layout component

--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -1,8 +1,7 @@
-// If you don't want to use TypeScript you can delete this file!
-import React from "react"
-import { PageProps, Link, graphql } from "gatsby"
+import React from "react";
+import { PageProps, Link, graphql } from "gatsby";
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout/Layout";
 import SEO from "../components/seo"
 
 type DataProps = {
