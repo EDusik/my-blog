4e97ee6ac8be8c10c4c536db516802f14361328b
EDusik <eduardodusik@gmail.com>
2021-02-03 08:25:20 -0300

    feat: adding SocialLinks component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Profile from "../Profile/Profile";
+import SocialLinks from "../SocialLinks/SocialLinks";
 
 import * as Styled from "./styled";
 
 const Sidebar = () => (
 	<Styled.SidebarWrapper>
 		<Profile />
+		<SocialLinks />
 	</Styled.SidebarWrapper>
 );
 
