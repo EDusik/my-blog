e9d2def09f13c816fc6363868fa268a7a691f49a
EDusik <eduardodusik@gmail.com>
2021-02-03 19:56:12 -0300

    feat(refactor): adding 'useStaticQuery' and 'graphql' in IndexPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,48 @@ import React from "react";
 import Layout from "../components/Layout/Layout";
 import PostItem from "../components/PostItem/PostItem";
 
+import { useStaticQuery, graphql } from "gatsby";
+
 import SEO from "../components/seo";
 
-const IndexPage = () => (
-	<Layout>
-		<SEO title="Home" />
-		<PostItem
-			slug="/about/"
-			category="Misc"
-			date="30 de Julho de 2019"
-			timeToRead="5"
-			title="Diga não ao Medium: tenha sua própria plataforma"
-			description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
-		/>
-	</Layout>
-);
+const IndexPage = () => {
+	const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              background
+              category
+              date(formatString: "MM/DD/YYYY")
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+  `);
+
+	const postList = allMarkdownRemark.edges;
+
+	return (
+		<Layout>
+			<SEO title="Home" />
+			{postList.map(({ node: { frontmatter: { background, category, date, description, title }, timeToRead } }) => (
+				<PostItem
+					slug="/about/"
+					background={background}
+					category={category}
+					date={date}
+					timeToRead={timeToRead}
+					title={title}
+					description={description}
+				/>
+			))}
+		</Layout>
+	);
+};
 
 export default IndexPage;
