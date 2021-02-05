c2f96c840a0c7cd5f2fe619d4a74edd4bf6aea93
EDusik <eduardodusik@gmail.com>
2021-02-05 09:16:10 -0300

    style: adding Post styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,24 +1,41 @@
 import React from "react";
 import { graphql } from "gatsby";
 
+import Layout from "../components/Layout/Layout";
+import SEO from "../components/seo";
+
+import * as Styled from "../components/Post/styled";
+
 const BlogPost = ({ data }) => {
 	const post = data.markdownRemark;
 
 	return (
-		<>
-			<h1>Title: {post.frontmatter.title}</h1>
-			<div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-		</>
+		<Layout>
+			<SEO title={post.frontmatter.title} />
+			<Styled.PostHeader>
+				<Styled.PostDate>
+					{post.frontmatter.date} • {post.timeToRead} minutes of reading
+				</Styled.PostDate>
+				<Styled.PostTitle>{post.frontmatter.title}</Styled.PostTitle>
+				<Styled.PostDescription>{post.frontmatter.description}</Styled.PostDescription>
+			</Styled.PostHeader>
+			<Styled.MainContent>
+				<div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+			</Styled.MainContent>
+		</Layout>
 	);
 };
 
 export const query = graphql`
-  query Post($slug: String) {
+  query Post($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        description
+        date(formatString: "MM/DD/YYYY")
       }
       html
+      timeToRead
     }
   }
 `;
